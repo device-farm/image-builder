92abcb0cb1ee28034302e82cf9749b9304348dfa
Pavel Burgr <pb@drake.cz>
2022-02-10 15:47:35 +0100

    WIP

--- builder/src/build.js
+++ builder/src/build.js
@@ -3,28 +3,26 @@ const fs = require("fs").promises;
 module.exports = async ({ exec }) => {
 
     const DEFAULT_BUILD_DIR = "/tmp/mic-build";
+    const DEFAULT_ACTION = "push";
+    const FRAGMENTS = ["cc", "boot", "kernel", "root", "installer"];
 
     return {
         name: "build <installer-image> <layers...>",
         description: "builds MIC Linux",
         define(program) {
             program
-                .option("-B, --build-dir <build-dir>", `directory for docker build context; defaults to ${DEFAULT_BUILD_DIR}`)
-            // .option("-D, --dry", "dry run - show docker command line only")
-            // .option("-w, --wifi <ssid:password>", "pre-configures WiFi connection")
-            // .option("-s, --ssh <pub-key-file>", "installs public SSH key for user root\nuse - as shorthand for ~/.ssh/id_rsa.pub")
-            // .option("-r, --root <overlay-dir>", "root overlay directory")
-            // .option("-t, --dto <dto-dir>", "device tree overlay directory")
-            // .option("-i, --installer <installer-image>", "installer docker image")
-            // .on("--help", () => {
-            //     console.info();
-            //     console.info("<blk-device> is SD card block device under /dev e.g. /dev/mmcblk0");
-            // });
+                .option("-B, --build-dir <dir>", `directory for docker build context; defaults to ${DEFAULT_BUILD_DIR}`)
+                .option("-D, --do-action <action>", `what to do with the image: push or load; defaults to ${DEFAULT_ACTION}`)
+                .option("-S, --stop-at <place>", `where to stop: ${[...FRAGMENTS, "build"].join(", ")}`)
         },
 
-        async run(installerImage, layers, { buildDir = DEFAULT_BUILD_DIR }) {
+        async run(installerImage, layers, {
+            buildDir = DEFAULT_BUILD_DIR,
+            doAction = DEFAULT_ACTION,
+            stopAt
+        }) {
 
-            console.info(`Building ${installerImage} in ${buildDir}`);
+            console.info(`Building ${installerImage}`);
             await exec("rm", ["-rf", buildDir]);
             await exec("mkdir", ["-p", buildDir]);
 
@@ -57,12 +55,105 @@ module.exports = async ({ exec }) => {
                 console.info(`Applying layer ${layerName}`);
                 await exec("cp", ["-r", layerDir + "/.", buildDir]);
 
+                async function scanAppends(dir) {
+                    for (let file of await fs.readdir(dir)) {
+                        let path = dir + "/" + file;
+                        let stat = await fs.stat(path);
+                        if (stat.isDirectory()) {
+                            await scanAppends(path);
+                        } else if (stat.isFile() && path.endsWith(".append")) {
+                            let data = await fs.readFile(path);
+                            let handle = await fs.open(path.replace(/\.append$/, ""), "a");
+                            try {
+                                await handle.write(data);
+                            } finally {
+                                await handle.close();
+                            }
+                            await fs.unlink(path);
+                        }
+                    }
+                }
+
+                await scanAppends(buildDir);
+
             }
 
+            layers = ["base", ...layers];
             for (let layer of layers) {
                 await applyLayer(layer);
             }
 
+            try {
+                await fs.unlink(buildDir + "/layer-deps");
+            } catch (e) {
+                if (e.code !== "ENOENT") {
+                    throw e;
+                }
+            }
+
+            let buildVars = {
+                BOARD_ID: installerImage
+            };
+
+            for (let varFile of await fs.readdir(buildDir)) {
+                if (varFile.match(/^\[[A-Z0-9_]+\]$/)) {
+                    let key = varFile.substring(1).substring(0, varFile.length - 2);
+                    let path = buildDir + "/" + varFile;
+                    let value = (await fs.readFile(path, "utf-8")).trim();
+                    buildVars[key] = value;
+                    await fs.unlink(path);
+                }
+            }
+
+            console.info("Build variables:");
+            for (let key in buildVars) {
+                console.info(` ${key}=${buildVars[key]}`);
+            }
+
+            let files = await fs.readdir(buildDir);
+            let content = "";
+            for (let fragment of FRAGMENTS) {
+                content += `
+##############################################
+# ${fragment}
+##############################################
+
+`;
+                let fragmentFiles = files.filter(file => file.startsWith(`Dockerfile-${fragment}`)).sort();
+                for (let fragmentFile of fragmentFiles) {
+                    let path = buildDir + "/" + fragmentFile;
+                    content += await fs.readFile(path, "utf-8");
+                    await fs.unlink(path);
+                }
+
+                if (stopAt === fragment) {
+                    break;
+                }
+            }
+
+            for (let key in buildVars) {
+                content = content.replace(new RegExp("\\$\\{" + key + "\\}", "g"), buildVars[key]);
+            }
+
+            content += Object.entries(buildVars).reduce((acc, [key, value]) => acc + "LABEL build." + key.toLowerCase().replace(/_/g, ".") + "=\"" + value + "\"\n", "")
+
+            await fs.writeFile(buildDir + "/Dockerfile", content, "utf-8");
+
+            console.info(`Build context ready in ${buildDir}`);
+
+            if (!stopAt) {
+
+                await exec("docker", [
+                    "buildx",
+                    "build",
+                    "--progress=plain",
+                    "--" + doAction,
+                    buildDir,
+                    "-t",
+                    installerImage
+                ]);
+
+            }
         }
     }
 }
