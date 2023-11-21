4346851d57d730d1fcef9a27ffb32e1e375b42c0
Pavel Burgr <pb@drake.cz>
2023-11-21 21:54:43 +0100

    WIP

--- layers/bcm2836/boot.cmd
+++
@@ -1,4 +0,0 @@
-setenv bootargs console=tty1 console=ttyAMA0,115200 root=/dev/mmcblk0p2 rw,noatime rootwait panic=10
-fatload mmc 0 ${fdt_addr_r} dtb
-fatload mmc 0 ${kernel_addr_r} zImage
-bootz ${kernel_addr_r} - ${fdt_addr_r}
